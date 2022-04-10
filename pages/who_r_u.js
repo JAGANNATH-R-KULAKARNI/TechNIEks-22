6a653bfd2389847dd3c97cc7471b089c57c2e5c4
Jagannath R Kulakarni <jagannathrkulakarni.171845@gmail.com>
2022-04-11 01:30:48 +0530

    google signin added

--- pages/who_r_u.js
+++ pages/who_r_u.js
@@ -1,7 +1,7 @@
 import NavBar from "../components/NavBar";
 import Footer from "../components/Footer";
 import useMediaQuery from "@mui/material/useMediaQuery";
-import SignInUI from "../components/signin/SignIn";
+import SignInUI from "../components/signin/SignIn2";
 
 export default function WhoRU() {
   const m1 = useMediaQuery("(min-width:600px)");
@@ -12,7 +12,6 @@ export default function WhoRU() {
       <div style={{ marginTop: !m1 ? "-20px" : "100px" }}>
         <SignInUI />
       </div>
-
       <div style={{ height: "50px" }}></div>
       <Footer />
     </div>
