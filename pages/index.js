622f4b094d619037a834f34b21317f09e791a119
Jagannath R Kulakarni <jagannathrkulakarni.171845@gmail.com>
2022-05-17 19:17:16 +0530

    almost over vro8

--- pages/index.js
+++ pages/index.js
@@ -22,16 +22,16 @@ export default function Home(props) {
 
         if (event === "SIGNED_IN") {
           setStatus(true);
-          Router.push("/events");
+          Router.push("/home");
         }
         if (event === "SIGNED_OUT") {
           setStatus(false);
-          Router.push("/events");
+          Router.push("/home");
         }
       }
     );
     checkUser();
-    Router.push("/events");
+    Router.push("/home");
     fetchTheProfile();
     return () => {
       authListener.unsubscribe();
