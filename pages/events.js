44fa3b3b9557558c735a661922f4c48b09280502
Jagannath R Kulakarni <jagannathrkulakarni.171845@gmail.com>
2022-05-23 14:11:21 +0530

    2 way sort

--- pages/events.js
+++ pages/events.js
@@ -10,87 +10,96 @@ import * as c from "../utils/Colors";
 import Cookies from "js-cookie";
 
 export default function Events() {
-  const m1 = useMediaQuery("(min-width:600px)");
-  const [status, setStatus] = React.useState(false);
-  const [events, setEvents] = React.useState([]);
-  const [gotit, setGotit] = React.useState(false);
+    const m1 = useMediaQuery("(min-width:600px)");
+    const [status, setStatus] = React.useState(false);
+    const [events, setEvents] = React.useState([]);
+    const [gotit, setGotit] = React.useState(false);
 
-  const router = useRouter();
-  const { dataEvents: errorEvents } = useSWR("events", fetchEvents);
-  const { dataProfile: errorProfile } = useSWR(
-    "profileEvents",
-    fetchTheProfile
-  );
+    const router = useRouter();
+    const { dataEvents: errorEvents } = useSWR("events", fetchEvents);
+    const { dataProfile: errorProfile } = useSWR(
+        "profileEvents",
+        fetchTheProfile
+    );
 
-  React.useEffect(() => {
-    fetchTheProfile();
-    fetchEvents();
-  }, []);
+    React.useEffect(() => {
+        fetchTheProfile();
+        fetchEvents();
+    }, []);
 
-  React.useEffect(() => {
-    setInterval(function () {
-      fetchTheProfile();
-    }, 100);
-  }, []);
+    React.useEffect(() => {
+        setInterval(function() {
+            fetchTheProfile();
+        }, 100);
+    }, []);
 
-  async function fetchTheProfile() {
-    const data = await supabase.auth.user();
-    setStatus(data ? true : false);
-  }
-
-  async function fetchEvents() {
-    const { data, error } = await supabase
-      .from("events")
-      .select("*")
-      .eq("type", 0)
-      .order("date", { ascending: true });
-
-    console.log("fetch events");
-    if (data) {
-      setEvents(data);
-      setGotit(true);
+    async function fetchTheProfile() {
+        const data = await supabase.auth.user();
+        setStatus(data ? true : false);
     }
 
-    if (error) {
-      // alert("Some Error Occurred :(, Try again later");
-      router.push("/home");
+    async function fetchEvents() {
+        const { data, error } = await supabase
+            .from("events")
+            .select("*")
+            .eq("type", 0)
+            .order("open", { ascending: false })
+            .order("date", { ascending: true });
+
+        console.log("fetch events");
+        if (data) {
+            setEvents(data);
+            setGotit(true);
+        }
+
+        if (error) {
+            // alert("Some Error Occurred :(, Try again later");
+            router.push("/home");
+        }
     }
-  }
 
-  async function logOut() {
-    await supabase.auth.signOut();
-    setStatus(false);
-    router.reload(window.location.pathname);
-  }
+    async function logOut() {
+        await supabase.auth.signOut();
+        setStatus(false);
+        router.reload(window.location.pathname);
+    }
 
-  return (
-    <div
-      style={{
-        backgroundColor: c.c1,
-        color: c.c2,
-        backgroundImage: "url(/images/logo3.png)",
-        backgroundPosition: m1 ? "center 80px" : "center 85px",
-        backgroundSize: m1 ? "450px 450px" : "250px 250px",
-        backgroundRepeat: "no-repeat",
-        backgroundAttachment: "fixed",
-        scrollBehavior: "smooth",
-      }}
-    >
-      <NavBar code={0} logOut={logOut} status={status} />
-      <TicketsUI events={events} status={status} />
-      {events.length > 0 ? null : (
-        <div
-          style={{ display: "flex", justifyContent: "center", height: "500px" }}
-        >
-          <h1
-            style={{ paddingTop: m1 ? "200px" : "60px", textAlign: "center" }}
-          >
-            {gotit ? "No Events" : "Loading..."}
-          </h1>
-        </div>
-      )}
-      {events.length > 0 ? null : <div style={{ height: "130px" }}></div>}
-      <Footer />
-    </div>
-  );
-}
+    return ( <
+        div style = {
+            {
+                backgroundColor: c.c1,
+                color: c.c2,
+                backgroundImage: "url(/images/logo3.png)",
+                backgroundPosition: m1 ? "center 80px" : "center 85px",
+                backgroundSize: m1 ? "450px 450px" : "250px 250px",
+                backgroundRepeat: "no-repeat",
+                backgroundAttachment: "fixed",
+                scrollBehavior: "smooth",
+            }
+        } >
+        <
+        NavBar code = { 0 }
+        logOut = { logOut }
+        status = { status }
+        /> <
+        TicketsUI events = { events }
+        status = { status }
+        /> {
+            events.length > 0 ? null : ( <
+                div style = {
+                    { display: "flex", justifyContent: "center", height: "500px" } } >
+                <
+                h1 style = {
+                    { paddingTop: m1 ? "200px" : "60px", textAlign: "center" } } >
+                { gotit ? "No Events" : "Loading..." } <
+                /h1> <
+                /div>
+            )
+        } {
+            events.length > 0 ? null : < div style = {
+                    { height: "130px" } } > < /div>} <
+                Footer / >
+                <
+                /div>
+        );
+    }
